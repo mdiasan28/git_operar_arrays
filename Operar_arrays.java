ba003dd420e69edf5dba9ce3d545100f6d4f5f3b
Miguel Ángel <mdiasan28@gmail.com>
2020-02-10 09:17:24 +0100

    version_1.2

--- Operar_arrays.java
+++ Operar_arrays.java
@@ -6,8 +6,8 @@ public class Operar_arrays {
 
 	public static void main(String[] args) {
 		int array[] =  Utils_arrays.CrearArrayNumAleatorio(10, 1, 20);
-//		MostrarArray(array);
-		
+		MostrarArray(array);
+		sumarArray(array);
 
 	}
 
@@ -23,14 +23,24 @@ public class Operar_arrays {
 		return array;
 	}
 	
-//	public static void MostrarArray (int [] array) {
-//		System.out.println("\nContenido del array");
-//		
-//		for (int i = 0; i < array.length; i++) {
-//			System.out.print(array[i] + " ");
-//			
-//		}
-//	}
+	public static void MostrarArray (int [] array) {
+		System.out.println("\nContenido del array");
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+			
+		}
+	}
+	
+	public static void sumarArray (int [] array) {
+		int numero=0;
+		int suma=0;
+		for( int i = 0; i <= 9; i++){
+	        numero = array[i];
+	        suma += numero;
+	    }
+		System.out.println("\nLa suma es: " + suma);
+	}
 	
 	public static int ObtenerNumAlteatorio (int limiteSup, int limiteInf) {
 		return (int) Math.round(Math.random()* (limiteSup - limiteInf)) + limiteInf;
